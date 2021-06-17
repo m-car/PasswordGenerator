ed26a4889bc3028a11f79ee4141290b0f96a9cba
Marko Caric <pbmc117@gmail.com>
2021-06-16 22:31:33 -0700

    Added original CSS file. Finished most of generatePassword function. Special characters do not work

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector('#generate');
-var pool=[];
+var pool=[]; //array of selected characters based on case/type
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var numbers = ['1','2','3','4','5','6','7','8','9','0']
+var special = ['!','@','#','$','%','^','&','*']
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -9,30 +14,61 @@ function writePassword() {
   passwordText.value = password;
 }
 
+function getRandomInt(max){
+  return Math.floor(Math.random() * max);
+}
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
 
-// wHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-if (numChar < 8 && numChar > 128){
 
-} 
+function generatePassword(){
+  // var pass ;
+  // Password length
+  var passLength = prompt("Enter required character length:")
+  if (passLength > 8 && passLength < 128){
+    
+    //Type/Case selection
+    var lower = confirm("lowercase?");
+    var upper = confirm("uppercase?");
+    var num = confirm("numeric?");
+    var spec = confirm("special?");
+  
+    //Builds array of available characters to choose from, based on previous task. 
+    if (lower){
+      pool = pool.concat(lowerCase);
+    }if(upper){
+      pool = pool.concat(upperCase);
+    }if(num){
+      pool = pool.concat(numbers);
+    }if(spec){
+      pool = pool.concat(special);
+    }else{
+      //what happens when all are false
+    }
 
-// prompt character type
-var lower = confirm("lowercase?")
-var upper = confirm("uppercase?")
-var num = confirm("numeric?")
-var spec = confirm("special?")
 
-// 
-var pool=[];
-if (lower){
-  pool.concat(lowerCase);
-}else if(upper){
-  pool.concat(upperCase);
-}else if(num){
-  pool.concat(numbers);
-}else if(spec){
-  pool.concat(special);
-}else{}
+    // TEST CASE TO PRINT POOL ARRAY
+    var poolstring = pool.toString();
+    console.log(poolstring);
+    // console.log(pool.length);
+    //TEST CASE 
 
+    var pass='';
+    var temp;
+    for (var i = 0; i< passLength; i++){
+      
+      //Random number in range of pool[] length
+      var index = getRandomInt(pool.length)
+      // select character in pool[], add to string 'pass'
+      temp = (pool[index]);
+      pass = pass.concat(temp);
+    }
+    console.log(pass);
+    return pass;
+  }else{
+    alert("Please choose between 8 and 128 characters.");
+    generatePassword(); 
+    // TODO I NEED ANOTHER ELSE CONDITION 
+  }
+}
+//TODO SPECIAL CHARACTERS NOT WORKING 
